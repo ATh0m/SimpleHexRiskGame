66e5312664c75569856d7c8f286280b53583bd45
AThom0x7cc <t.nanowski@gmail.com>
2016-01-04 21:51:24 +0100

    Added simple player and better drawing

--- game.c
+++ game.c
@@ -4,11 +4,15 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+typedef struct Color{
+	int r, g, b, a;
+} Color;
 
-void draw_filled_hex( SDL_Renderer *renderer, int x, int y, int size );
+void draw_filled_hex( SDL_Renderer *renderer, int x, int y, int size, Color color);
 
-void draw_hex( SDL_Renderer *renderer, int x, int y, int size );
+void draw_hex( SDL_Renderer *renderer, int x, int y, int size, Color color);
 
 SDL_Window *create_window ( int width, int height );
 
@@ -24,6 +28,8 @@ typedef struct Pair{
 	int x, y;
 } Pair;
 
+enum Status {CREATE, START, REINFORCEMENT, MOVE};
+
 typedef struct Board
 {
 	Field **fields;
@@ -36,6 +42,8 @@ typedef struct Board
 	int offset_x;
 	int offset_y;
 	
+	enum Status status;
+	
 	Pair hover_field;
 } Board;
 
@@ -45,14 +53,10 @@ void draw_board( SDL_Renderer *renderer, Board *board );
 
 void draw ( SDL_Renderer *renderer, Board *board );
 
-enum Status {CREATE, START, REINFORCEMENT, MOVE};
-
-typedef struct Color{
-	int r, g, b, a;
-} Color;
-
 typedef struct Player{
 	char *name;
+	int id;
+	
 	bool active;
 	bool ai;
 	
@@ -63,7 +67,12 @@ typedef struct Player{
 	Color action_color;
 } Player;
 
-// TO DO bool is_actionable(Board *board, int x, int y, PLAYER, State);
+bool is_actionable(Board *board, int x, int y, Player *player);
+
+Color create_color(int r, int g, int b, int a) {
+	Color new_color = {r, g, b, a};
+	return new_color;
+}
 
 Pair point_to_field (int x, int y, Board *board) {
 	Pair field;
@@ -72,6 +81,15 @@ Pair point_to_field (int x, int y, Board *board) {
     field.x = floor(((field.y % 2 == 0 ? x : x - board->field_size) - board->offset_x) / board->field_width);
     
     return field;
+}
+
+Pair field_to_point (int x, int y, Board *board) {
+	Pair point;
+	
+	point.y = y * board->field_height + board->field_height * 0.5 + board->offset_y;
+	point.x = (y % 2 == 0 ? 0 : board->field_width * 0.5) + x * board->field_width + board->field_width * 0.5 + board->offset_x;
+	
+	return point;
 }
 
 int main ( int argc, char** argv )
@@ -84,10 +102,21 @@ int main ( int argc, char** argv )
 	int quit = false;
 	SDL_Event e;
 	
-	status = CREATE;
-	
 	int tab[5][4] = {{1, 1, 0, 1}, {1, 0, 0, 1}, {1, 1, 1, 1}, {1, 0, 1, 0}, {1, 0, 1, 0}};
 	Board *board = create_board(5, 4, tab);
+	
+	Player* players[3];
+	
+	players[0] = malloc(sizeof(Player));
+	players[0]->name = strdup("Red");
+	players[0]->id = 0;
+	players[0]->active = false;
+	players[0]->ai = false;
+	players[0]->field_color = create_color(255, 255, 255, 255);
+	players[0]->hover_color = create_color(0, 0, 0, 0);
+	players[0]->action_color = create_color(0, 0, 0, 0);
+	
+	board->status = START;
 	
 	while ( !quit ) 
 	{
@@ -118,6 +147,18 @@ int main ( int argc, char** argv )
 	return EXIT_SUCCESS;
 }
 
+bool is_actionable(Board *board, int x, int y, Player *player) {
+	if (x >= 0 && x < board->width && y >= 0 && y < board->height && board->fields[x][y].owner >= 0) {
+		if(board->status == START) {
+			if (board->fields[x][y].owner == 0) {
+				return true;
+			}
+		}
+	}
+	
+	return false;
+}
+
 Board *create_board(int cols, int rows, int tab[cols][rows]) 
 {
 	Board *new_board = malloc(sizeof(Board));
@@ -138,16 +179,17 @@ Board *create_board(int cols, int rows, int tab[cols][rows])
 	return new_board;
 }
 
-void draw_filled_hex( SDL_Renderer *renderer, int x, int y, int size ) 
+void draw_filled_hex( SDL_Renderer *renderer, int x, int y, int size, Color color) 
 {
 
 	const short vx[6] = { x - size, x - size, x, x + size, x + size, x };
 	const short vy[6] = { y + size * 0.5, y - size * 0.5, y - size, y - size * 0.5, y + size * 0.5, y + size };
 
-	filledPolygonRGBA( renderer, vx, vy, 6, 128, 128, 128, 255 );
+	//filledPolygonRGBA( renderer, vx, vy, 6, 128, 128, 128, 255 );
+	filledPolygonRGBA( renderer, vx, vy, 6, color.r, color.g, color.b, color.a );
 }
 
-void draw_hex( SDL_Renderer *renderer, int x, int y, int size ) 
+void draw_hex( SDL_Renderer *renderer, int x, int y, int size, Color color) 
 {
 	const short vx[6] = { x - size, x - size, x, x + size, x + size, x };
 	const short vy[6] = { y + size * 0.5, y - size * 0.5, y - size, y - size * 0.5, y + size * 0.5, y + size };
@@ -157,7 +199,8 @@ void draw_hex( SDL_Renderer *renderer, int x, int y, int size )
 
 	for ( int i = 0; i < 6; i++ )
 	{
-		thickLineRGBA (renderer, last_x, last_y, vx[i], vy[i], 5, 255, 51, 51, 255);
+		// thickLineRGBA (renderer, last_x, last_y, vx[i], vy[i], 5, 255, 51, 51, 255);
+		thickLineRGBA (renderer, last_x, last_y, vx[i], vy[i], 5, color.r, color.g, color.b, color.a);
 
 		last_x = vx[i];
 		last_y = vy[i];
@@ -187,11 +230,23 @@ void draw_board( SDL_Renderer *renderer, Board *board )
 				// int hx = 2 * board->field_size * x + board->field_size;
 				// if (y % 2 == 1) hx += board->field_size;
 				
-				int hy = y * board->field_height + board->field_height * 0.5 + board->offset_y;
-				int hx = (y % 2 == 0 ? 0 : board->field_width * 0.5) + x * board->field_width + board->field_width * 0.5 + board->offset_x;
-
-				draw_filled_hex( renderer, hx, hy, board->field_size - 2 );
-				if (x == board->hover_field.x && y == board->hover_field.y) draw_hex( renderer, hx, hy, board->field_size );
+				Color color = {128, 128, 128, 255};
+				
+				Pair point = field_to_point(x, y, board);
+				draw_filled_hex( renderer, point.x, point.y, board->field_size - 2, color);
+	
+				Player *player;
+				
+				Color color2 = {255, 102, 102, 255};
+				
+				Color color3 = {255, 51, 51, 255};
+		
+				if (is_actionable(board, x, y, player)) {
+					if (x == board->hover_field.x && y == board->hover_field.y ) {
+						draw_filled_hex( renderer, point.x, point.y, board->field_size - 2, color2);
+					}
+					draw_hex( renderer, point.x, point.y, board->field_size, color3);
+				}
 			}
 		}
 	}
