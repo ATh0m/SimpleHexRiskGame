32fa37d534308e15cc7d05b59b4bc1a385a5f6bc
AThom0x7cc <t.nanowski@gmail.com>
2016-01-04 21:00:03 +0100

    Added mouse event

--- game.c
+++ game.c
@@ -20,11 +20,23 @@ typedef struct Field
 	int force;
 } Field;
 
+typedef struct Pair{
+	int x, y;
+} Pair;
+
 typedef struct Board
 {
 	Field **fields;
-	int field_size;
 	int width, height;
+	
+	int field_size;
+	int field_width;
+	int field_height;
+	
+	int offset_x;
+	int offset_y;
+	
+	Pair hover_field;
 } Board;
 
 Board *create_board(int cols, int rows, int tab[cols][rows]);
@@ -33,15 +45,46 @@ void draw_board( SDL_Renderer *renderer, Board *board );
 
 void draw ( SDL_Renderer *renderer, Board *board );
 
+enum Status {CREATE, START, REINFORCEMENT, MOVE};
+
+typedef struct Color{
+	int r, g, b, a;
+} Color;
+
+typedef struct Player{
+	char *name;
+	bool active;
+	bool ai;
+	
+	// TO DO List fields
+	
+	Color field_color;
+	Color hover_color;
+	Color action_color;
+} Player;
+
+// TO DO bool is_actionable(Board *board, int x, int y, PLAYER, State);
+
+Pair point_to_field (int x, int y, Board *board) {
+	Pair field;
+	
+    field.y = floor((y - board->offset_y) / board->field_height);
+    field.x = floor(((field.y % 2 == 0 ? x : x - board->field_size) - board->offset_x) / board->field_width);
+    
+    return field;
+}
 
 int main ( int argc, char** argv )
 {
 	
 	SDL_Window *window = create_window( 640, 480 );
 	SDL_Renderer *renderer = crete_renderer( window );
+	enum Status status;
 	
 	int quit = false;
 	SDL_Event e;
+	
+	status = CREATE;
 	
 	int tab[5][4] = {{1, 1, 0, 1}, {1, 0, 0, 1}, {1, 1, 1, 1}, {1, 0, 1, 0}, {1, 0, 1, 0}};
 	Board *board = create_board(5, 4, tab);
@@ -54,6 +97,13 @@ int main ( int argc, char** argv )
 			if( e.type == SDL_QUIT ) 
 			{
 				quit = true;
+			}
+			if ( e.type == SDL_MOUSEMOTION)
+			{
+				int x, y;
+				SDL_GetMouseState( &x, &y );
+				
+				board->hover_field = point_to_field(x, y, board);
 			}
 		}
 		
@@ -119,7 +169,13 @@ void draw_board( SDL_Renderer *renderer, Board *board )
 
 	int w, h;
 	SDL_GetRendererOutputSize( renderer, &w, &h );
-	board->field_size = h * 2 / (1 + 3 * board->width);
+	board->field_size = w / (2 * (0.5 + board->width));
+	
+	board->field_width = board->field_size * 2;
+	board->field_height = board->field_width * 0.75;
+	
+	board->offset_x = (w - board->width * board->field_width - board->field_width * 0.5) * 0.5;
+	board->offset_y = board->field_width * 0.125;
 
 	for (int x = 0; x < board->width; x++) 
 	{
@@ -127,12 +183,15 @@ void draw_board( SDL_Renderer *renderer, Board *board )
 		{
 			if (board->fields[x][y].owner >= 0) 
 			{
-				int hy = y * board->field_size * 3 / 2 + board->field_size;
-				int hx = 2 * board->field_size * x + board->field_size;
-				if (y % 2 == 1) hx += board->field_size;
+				// int hy = y * board->field_size * 3 / 2 + board->field_size;
+				// int hx = 2 * board->field_size * x + board->field_size;
+				// if (y % 2 == 1) hx += board->field_size;
+				
+				int hy = y * board->field_height + board->field_height * 0.5 + board->offset_y;
+				int hx = (y % 2 == 0 ? 0 : board->field_width * 0.5) + x * board->field_width + board->field_width * 0.5 + board->offset_x;
 
 				draw_filled_hex( renderer, hx, hy, board->field_size - 2 );
-				if (x == 2 && y == 3) draw_hex( renderer, hx, hy, board->field_size );
+				if (x == board->hover_field.x && y == board->hover_field.y) draw_hex( renderer, hx, hy, board->field_size );
 			}
 		}
 	}
